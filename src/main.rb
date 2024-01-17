4c8467c645e748aae8475ef52ddaf29369a856fa
Oğuz Kayra Büyükyıldırım <74321096+OguzKaira@users.noreply.github.com>
2024-01-17 22:39:30 +0300

    Update main.rb

--- src/main.rb
+++ src/main.rb
@@ -11,6 +11,16 @@ def find_files_and_folders(files , directories)
     end
 end
 
+def convert_files_to_python(files)
+    # Get file in files 
+    for file in files do
+        # Split file name to get extension and name of the file
+        file_name = file.split('.')
+        # Rename the original file (change extension of this file to rb)
+        File.rename(Dir.pwd + '/%s' % [file] , Dir.pwd + '/%s.rb' % [file_name[0]])
+    end
+end
+
 # Store in 2 different arrays 
 files , directories = [] , []
 find_files_and_folders(files , directories)
